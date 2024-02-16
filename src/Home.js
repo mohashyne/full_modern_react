231e25d663cb4d0186597b8878433ea9f37b0c08
MSA <crystalbluetech@gmail.com>
2024-02-16 16:41:14 +0100

    using hooks(state) to change behaviour

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,33 @@
+import { useState } from "react";
+
 const Home = () => {
+    // let name = "Muhammad";
   
-    const handleClick = (e) => {
-        console.log('hello, ninjas', e);
+    // this will not change the name because its not reactive
+    // we need to use a hook(useState)
+    // const handleClick = () => {
+    //    name = "Salihu";
+    //    console.log(name);
+    // }
+   
+    // Now this is  reactive
+    const [name, setName] = useState('Muhammad');
+    const [age, setAge] = useState(28);
+
+    const handleClick = () => {
+        setName('Salihu');
+        if(age > 25) { setAge(30);}
+        else {setAge(38);}
     }
 
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target);
-    }
-
+    
 
 
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <p>{name} is { age } years old</p>
             <button onClick={handleClick}>Click me!</button>
-            <button onClick={(e) => {
-                handleClickAgain('Muhammad', e)
-            }} >Click me again</button>
         </div>
      );
 }
