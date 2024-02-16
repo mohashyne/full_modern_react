2da852fe50cf0c3cd02f11214158f64356310ecf
MSA <crystalbluetech@gmail.com>
2024-02-17 00:31:10 +0100

    add blog list

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,27 @@
 import { useState } from "react";
 
 const Home = () => {
-    // let name = "Muhammad";
-  
-    // this will not change the name because its not reactive
-    // we need to use a hook(useState)
-    // const handleClick = () => {
-    //    name = "Salihu";
-    //    console.log(name);
-    // }
-   
-    // Now this is  reactive
-    const [name, setName] = useState('Muhammad');
-    const [age, setAge] = useState(28);
+    const [blogs, setBlogs] = useState([
+        { title: 'First Blog', body: 'This is the first blog.', author: 'John Doe', id:1 },
+        { title: 'Second Blog', body: 'This is the Second blog.', author: 'MSA', id:2 },
+        { title: 'Third Blog', body: 'This is the Third blog.', author: 'hauwa', id:3 }
 
-    const handleClick = () => {
-        setName('Salihu');
-        if(age > 25) { setAge(30);}
-        else {setAge(38);}
-    }
-
-    
+    ]);
 
 
     return ( 
         <div className="home">
-            <h2>Home Page</h2>
-            <p>{name} is { age } years old</p>
-            <button onClick={handleClick}>Click me!</button>
+           {
+            blogs.map(blog => {
+                return (
+                    <div key={blog.id} className='blog-preview'>
+                        <h2>{blog.title}</h2>
+                        <p>{blog.body}</p>
+                        <span>Written By: {blog.author}</span>
+                    </div>
+                )
+            })
+           } 
         </div>
      );
 }
