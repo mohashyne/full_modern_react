bbea9e6bfd3ef00ef5f5dfec994117927e1ac076
MSA <crystalbluetech@gmail.com>
2024-02-23 12:05:11 +0100

    useeffect to run on specified render only

--- src/Home.js
+++ src/Home.js
@@ -9,22 +9,27 @@ const Home = () => {
 
     ]);
 
+    const [name, setName] = useState('hauwa');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     };
     
-       
+    //  using dependency array to run the use effect only when the component mounts.  
     useEffect(() => {
      console.log('use effect ran');
-     console.log(blogs);
-    });
+    //  console.log(blogs);
+    console.log(name);
+    }, [name]);  // Only re-run if `blogs` changes, if it is an empty array so it only runs once when the component mounts
 
 
     return ( 
         <div className="home">
            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
            <BlogList blogs={blogs.filter((blog) => blog.author === "hauwa")} title="Hauwa's Blogs" handleDelete={handleDelete}/>
+           <button onClick={() => setName("Afra")} >Change name</button>
+           <p>{ name }</p>
         </div>
      );
 }
